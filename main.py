410bb047342342175c79b738a7868bef75c90956
Malik Bougacha <malik.bougacha@optiopay.com>
2020-04-24 13:36:45 +0200

    more chtuff

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
 import requests
-
 from bs4 import BeautifulSoup
-
-# class Project
-#     def __init__(project):
-#         dum,self.author,self.title = project.h1.a['href'].split("/")
-
-#         self.description = project.p.text.strip()
-
-#         __str__():
 
 
 html_page = requests.get('https://github.com/trending')
@@ -19,30 +10,23 @@ html_txt = html_page.text
 soup = BeautifulSoup(html_txt, 'html.parser')
 names = soup.findAll("div", { "class" : "Box" })
 
-projects = soup.findAll('article', {"class" : "Box-row"})
+projects = soup.findAll('article')
 
 
 for project in projects:
-
+    
     dum,author,title = project.h1.a['href'].split("/")
+    print(title)
+    print(author)
     description = project.p.text.strip()
+    print(description)
     # stars = project.findAll("div span")
-    infospans = project.findAll("div") #.select("span") # stars[0].findChildrens()
-    print(infospans)
-    # language = infospans[0].find(span).text()
-    # print(language)
-    # oneStar = stars.findAll('a', {"class" : " muted-link d-inline-block mr-3"})
-    
-    
-    
-    
-    # stars = project.findAll(div[2]/a[1])
-    # forks =
-    # language =
-    # print(author)
-    # print(title)
-    # print(description)
-    # print(stars)
-    # print(oneStar)
-
-    exit()
+    raw_language = project.find("span", itemprop="programmingLanguage")
+    if raw_language:
+        print(raw_language.text)
+    raw_star, raw_fork, *_ = project.findAll('a', {"class": "d-inline-block"})
+    if raw_star:
+        print("".join(raw_star.text.split()).replace(",", ""))
+    if raw_fork
+    print(raw_star)
+    print('-'*10)
