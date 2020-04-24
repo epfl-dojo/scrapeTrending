0ba1790b63f73a77d7ec698c1bc84bd898ba92f5
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2020-04-24 14:23:01 +0200

    Start arg parser

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
-import requests, collections
+import argparse
+import collections
+import json
+import requests 
+
 from bs4 import BeautifulSoup
 
 ProjectDetail = collections.namedtuple("ProjectDetail", ["author", "title", "description", "language", "star", "fork", "url"])
+
+parser = argparse.ArgumentParser(description="Fetch github trending repos")
+parser.add_argument('--since', default='today', help="set time range")
+
+args = parser.parse_args()
+print(args.since)
 
 html_page = requests.get('https://github.com/trending')
 html_txt = html_page.text
@@ -11,17 +21,12 @@ html_txt = html_page.text
 soup = BeautifulSoup(html_txt, 'html.parser')
 names = soup.findAll("div", { "class" : "Box" })
 
-projects = soup.findAll('article')
-
-
-for project in projects:
+projects = []
+for project in soup.findAll('article'):
 
     dum, author, title = project.h1.a['href'].split("/")
 
-    print(title)
-    print(author)
     description = project.p.text.strip()
-    print(description)
     # stars = project.findAll("div span")
     raw_language = project.find("span", itemprop="programmingLanguage")
     if raw_language:
@@ -40,8 +45,7 @@ for project in projects:
     else:
         fork = "not found"
 
-    print('-'*10)
+    projects.append(ProjectDetail(author=author, title=title, description=description, language=language, star=star, fork=fork, url=''))
 
-    ProjectDetail(author=author, title=title, description=description, language=language, start=star, fork=fork, url='')
-    
-    
+print(json.dumps(projects))
+	    
